e6249a345237a536b134afacc4911069aae2bc5f
J. Jesús Espinoza P <37966585+Yisus2808@users.noreply.github.com>
2024-01-29 11:08:36 -0700

    Bonus Track
    Se mejoro el listado de los clientes con paginator.

--- src/main/java/com/bolsadeideas/springboot/app/models/service/IClienteService.java
+++ src/main/java/com/bolsadeideas/springboot/app/models/service/IClienteService.java
@@ -1,5 +1,7 @@
 package com.bolsadeideas.springboot.app.models.service;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import java.util.List;
 
 import com.bolsadeideas.springboot.app.models.entity.Cliente;
@@ -7,6 +9,8 @@ import com.bolsadeideas.springboot.app.models.entity.Cliente;
 public interface IClienteService {
 	
 	public List<Cliente> findAll();
+	
+	public Page<Cliente> findAll(Pageable pageable);
 
 	public void save(Cliente cliente);
 
